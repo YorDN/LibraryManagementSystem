32c91b88d4862fcf4de10415ff469e78c3b088f0
Yordan Yordanov <mixdata34@gmail.com>
2025-03-24 18:08:51 +0200

    Added the publisher model

--- LibrarySystem.Data/Models/Publisher.cs
+++ LibrarySystem.Data/Models/Publisher.cs
@@ -1,9 +1,30 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 
+using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystem.Data.Models
 {
     class Publisher
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("The name of the publisher")]
+        public required string Name { get; set; }
+
+        [Required]
+        [Comment("The logo of the publisher")]
+        public required string Logo { get; set; }
+
+        [Comment("The country of the publisher")]
+        public string? Country { get; set; }
+
+        [Comment("The website of the publisher")]
+        public string? Website { get; set; }
+
+        [Comment("A collection of the books published by the publisher")]
+        public ICollection<Book> Books { get; set; } = new List<Book>();
     }
 }
