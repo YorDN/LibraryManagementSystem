6c03387607a3cd5afe0cae8035628354947e18ca
Yordan Yordanov <mixdata34@gmail.com>
2025-03-24 18:13:23 +0200

    Added the borrow model.

--- LibrarySystem.Data/Models/Borrow.cs
+++ LibrarySystem.Data/Models/Borrow.cs
@@ -1,8 +1,31 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+
 namespace LibrarySystem.Data.Models
 {
     class Borrow
     {
-        public int MyProperty { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+        public Book Book { get; set; } = null!;
+
+        [Comment("When was the book taken")]
+        public DateOnly DateTaken { get; set; }
+
+        [Comment("When does the book have to be returned")]
+        public DateOnly DateDue { get; set; }
+
+        [Comment("Is the book returned")]
+        public bool Returned { get; set; }
     }
 }
