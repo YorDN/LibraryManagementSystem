05733c51c299547c5ee390ee9e70957ffdafa587
Yordan <mixdata34@gmail.com>
2025-03-24 21:38:00 +0200

    Changed the logic behund the user model and inherited IdentityUser from the Microsoft.AspNetCore.Identity package

--- LibrarySystem.Data/Models/User.cs
+++ LibrarySystem.Data/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystem.Data.Models
@@ -8,43 +9,14 @@ namespace LibrarySystem.Data.Models
     /// <summary>
     ///  This is the User model, which represents a user in the LMS
     /// </summary>
-    class User
+    class User : IdentityUser
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
-        [Comment("The user's username")]
-        public required string Username { get; set; }
         
-        [Required]
-        [MaxLength(255)]
-        [Comment("The user's password")]
-        public required string Password { get; set; }
-
-        [Required]
-        [MaxLength(320)]
-        [RegularExpression(@"^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
-        [Comment("The user's email address")]
-        public required string Email { get; set; }
-
         [Required]
         [Comment("The user's profile picture")]
         // The profile picture is required because every user must have one 
         // If the user doesn't have a profile picture, a default profile picture will be used
         public required string ProfilePicture { get; set; }
-
-        public enum UserRoles
-        {
-            User,
-            Admin
-        }
-
-        [Required]
-        [Comment("The user's role")]
-        // There are 2 roles: User and Admin
-        public UserRoles Role { get; set; } = UserRoles.User;
 
         [Required]
         [Comment("When was the user created")]
@@ -56,5 +28,8 @@ namespace LibrarySystem.Data.Models
 
         [Comment("A collection of the books taken by the user")]
         public ICollection<Borrow> Borrows { get; set; } = new List<Borrow>();
+
+        [Comment("A collection of the reviews written by the user")]
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
     }
 }
