ab05d0112eb5b78f4fa908bfa0f13bf170283fbb
Yordan <mixdata34@gmail.com>
2025-03-28 14:19:00 +0200

    Added a quote model

---
+++ LibrarySystem.Data/Models/Quote.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Data.Models
+{
+    /// <summary>
+    ///     This is the quote model, which is used for the quotes on the books page
+    /// </summary>
+    public class Quote
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("The text of the quote")]
+        public required string QuoteText { get; set; }
+
+        [Required]
+        [Comment("The name of the author, that said the quote")]
+        public required string AuthorName { get; set; }
+    }
+}
